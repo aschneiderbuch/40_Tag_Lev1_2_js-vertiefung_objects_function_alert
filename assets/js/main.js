35cbb3ab15cd0210b4ee1991d1596e42cc77a774
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-07 13:16:48 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,25 @@ const person ={
     name: "Max",
     alter: 28,
     sagNameAlter: function(){
-       alert( `${this.name} ${this.alter}`) // geht
-       alert(this.name + " " + this.alter)  // geht
-       alert(person.name + " " + person.alter)
+      // alert( `${this.name} ${this.alter}`) // geht
+       // alert(this.name + " " + this.alter)  // geht
+      // alert(person.name + " " + person.alter)
        console.log((person.name + " " + person.alter))
+    },
+    kind: {
+        augen: "braun",
+        beine: 2,
+        finger: [1,2,3,4,5],
+        fuesse: "links rechts"
+
+
     }
 }
 
 person.sagNameAlter()
+
+console.log(person.kind.augen) // braun 
+console.log(person.kind.beine) // 2
+console.log(person.kind.finger[4])  // 5 
+console.log(person.kind.fuesse.split(' '))  // aus String wird -> Array erzeugt mit [links, rechts]
+console.log(person.kind.fuesse.slice(0,5)) // String bleibt -> String  links   
