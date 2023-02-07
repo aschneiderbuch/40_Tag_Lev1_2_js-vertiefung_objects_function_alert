e9cea4bde27a658ae78ad1cead78be3f7db125be
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-07 13:04:22 +0100

    testingPhase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 console.log("test");
+
+/* 
+
+objekt person erstellen
+Eigenschaft name -> key  : value ;
+
+*/
+
+const person ={
+    name: "Max",
+    alter: 28,
+    sagNameAlter: function(){
+       alert( `${this.name} ${this.alter}`) // geht
+       alert(this.name + " " + this.alter)  // geht
+       alert(person.name + " " + person.alter)
+       console.log((person.name + " " + person.alter))
+    }
+}
+
+person.sagNameAlter()
